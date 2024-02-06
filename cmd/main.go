25995005409be66ee8175b274f8060ab1674d8b1
anshry <faqih49@gmail.com>
2024-02-06 15:27:12 +0700

    finished course

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"html/template"
 	"io"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -23,24 +25,133 @@ func newTemplate() *Template {
 	}
 }
 
+type Contact struct {
+	Name string
+	Email string
+	Id int
+}
+
+func newContact(name, email string) Contact {
+	id++
+	return Contact{
+		Name: name,
+		Email: email,
+		Id: id,
+	}
+}
+
+var id = 0
+type Contacts = []Contact
+
+func newData() Data {
+	return Data {
+		Contacts: []Contact{
+			newContact("John Doe", "jd@gmail.com"),
+			newContact("Jane Doe", "cd@gmail.com"),
+		},
+	}
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData {
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data 
+	Form FormData
+}
+
+func newPage() Page {
+	return Page {
+		Data: newData(),
+		Form: newFormData(),
+	}
+}
+
+type Data struct {
+	Contacts Contacts
+}
+
+func (d *Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
 type Count struct {
 	Count int
+}
+
+func (d *Data) indexOf(id int) int {
+	for i , contact := range d.Contacts {
+		if contact.Id == id {
+			return i
+		}
+	}
+	return -1
 }
 
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	count := Count { Count: 0 }
+	page := newPage()
 	e.Renderer = newTemplate()
 
+	e.Static("/images", "images")
+	e.Static("/css", "css")
+
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "index", count)
+		return c.Render(http.StatusOK, "index", page)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(http.StatusOK, "count", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+
+			return c.Render(http.StatusUnprocessableEntity, "form", formData)
+		} 
+
+		contact := newContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+
+		c.Render(http.StatusOK, "form", newFormData())
+		return c.Render(http.StatusOK, "oob-contact", contact)
+	})
+
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		time.Sleep(2 * time.Second)
+
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return c.String(http.StatusBadRequest, "Invalid ID")
+		}
+
+		index := page.Data.indexOf(id)
+		if index == -1 {
+			return c.String(http.StatusNotFound, "Contact not found")
+		} 
+
+		page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+		return c.NoContent(http.StatusOK)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
